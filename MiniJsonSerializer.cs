78d571920dc64bcaeb0b9261c9e07bee4287bbe4
Crestfall <mas025@edu.zealand.dk>
2026-09-17 10:24:21 +0200

    Add project files.

---
+++ MiniJsonSerializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniJsonSerializer
+{
+    public class MiniJsonSerializer
+    {
+        public static string Serialize(object obj)
+        {
+
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MiniJsonSerializer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person
+            {
+                Name = "Morten",
+                Age = 27,
+                Student = true
+            };
+        }
+    }
+}
